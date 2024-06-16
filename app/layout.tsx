2cd5364bca28ce2a31a871a2709704a9540ab5f4
Lalit sharma <geekymonkcp742k@gmail.com>
2024-06-16 18:43:53 +0530

    Added Next themes and mode toggle on navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,12 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
 import Navbar from "@/components/Navbar";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Aura - Transcribe audio in minutes",
+    title: "WhispX - Transcribe audio in minutes",
     description: "Transcribe audio in minutes with WhisperV3 and Flash Attention v2 + Transformers without relying on third-party providers and APIs. Host it yourself or try it out.",
 };
 
@@ -18,10 +19,18 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+
             <body className={inter.className}>
-                <Navbar />
-                {children}
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="dark"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    <Navbar />
+                    {children}
+                </ThemeProvider>
             </body>
-        </html>
+        </html >
     );
 }
