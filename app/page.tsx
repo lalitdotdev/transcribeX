6ec890d60a71e0025bd84f8e9137a0681a71c50f
Lalit sharma <geekymonkcp742k@gmail.com>
2024-06-17 19:44:54 +0530

    Add spotlight on home

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
+import CodeHost from "@/components/codehost/code-host";
 import Image from "next/image";
 import Link from "next/link";
+import { Spotlight } from "@/components/ui/spotlight";
 import { buttonVariants } from "@/components/ui/button";
 import { twMerge } from "tailwind-merge";
 
 export default function Home() {
     return (
-        <div className="container flex w-full flex-col items-center gap-12">
+        <div className="container flex w-full flex-col items-center gap-12 antialiased  relative overflow-hidden">
+
             <section className="text-center py-28 max-w-3xl flex flex-col gap-3 items-center">
+                <Spotlight
+                    className="-top-40 left-0 md:left-60 md:-top-20"
+                    fill="white"
+                />
                 <h1 className="pt-24 text-5xl sm:text-6xl tracking-tighter font-semibold">
                     Transcribe audio in minutes with WhisperV3
                 </h1>
@@ -37,7 +44,7 @@ export default function Home() {
                 id="host"
                 className="text-center py-20 sm:py-32 max-w-3xl flex flex-col gap-3 items-center"
             >
-                {/* <CodeHost /> */}
+                <CodeHost />
             </section>
         </div>
     );
