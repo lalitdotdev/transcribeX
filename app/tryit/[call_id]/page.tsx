cc586f02b739d845838bd5b262892260cc4171aa
Lalit sharma <geekymonkcp742k@gmail.com>
2024-06-17 20:19:08 +0530

    🎉 feat(call_id page data fetching from entrypoint running on modal): Integrated loading, progress, and failure states (Loading, ReqProgress, ReqFail) for better user experience during data fetching and processing.

--- app/tryit/[call_id]/page.tsx
+++ app/tryit/[call_id]/page.tsx
@@ -12,6 +12,51 @@ export default function Page({ params }: { params: { call_id: string } }) {
     const [data, setData] = useState<any | undefined>();
     const [status, setStatus] = useState<number>();
 
+    useEffect(() => {
+        let timeoutId: NodeJS.Timeout;
+        const formData = new FormData();
+        formData.append("call_id", params.call_id);
+        const fetchData = async () => {
+            fetch(
+                `https://lalitdotdev--transcribe-x-entrypoint.modal.run/call_id`,
+                {
+                    method: "POST",
+                    body: formData,
+                }
+            )
+                .then((response) => {
+                    setStatus(response.status);
+                    if (response.status == 202) {
+                        timeoutId = setTimeout(fetchData, 10000);
+                    } else {
+                        return response.json();
+                    }
+                })
+                .then((data) => {
+                    setData(data);
+                })
+                .catch((error) => {
+                    setStatus(500);
+                });
+        };
+
+        fetchData();
+
+        return () => clearTimeout(timeoutId);
+    }, []);
+
+    if (status == 202) {
+        return <ReqProgress />;
+    }
+
+    if (status == 500) {
+        return <ReqFail />;
+    }
+
+    if (!data) {
+        return <Loading />;
+    }
+
     if (data) {
         return (
             <>
